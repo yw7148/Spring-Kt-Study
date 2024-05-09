8e2cf90ca44c40e18a60fb03432751cf150d308a
Youngwon <71220342+yw7148@users.noreply.github.com>
2024-05-09 13:06:18 +0900

    spring kt init

---
+++ src/main/kotlin/me/youngwon/ktstudy/KtstudyApplication.kt
@@ -0,0 +1,11 @@
+package me.youngwon.ktstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtstudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtstudyApplication>(*args)
+}
---
+++ src/test/kotlin/me/youngwon/ktstudy/KtstudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.youngwon.ktstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtstudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
